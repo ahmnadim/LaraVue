6ecfe735a058cb2c465e4213be65b71d212faa9b
Ahmad Ul Hoq Nadim <ahmadulhoqnadim@gmail.com>
2019-03-18 14:30:48 +0600

    added user modal input field

--- resources/js/components/UsersComponent.vue
+++ resources/js/components/UsersComponent.vue
@@ -35,6 +35,51 @@
                         </a>
                     </td>
                   </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#" class="btn btn-primary">
+                            <i class="fa fa-edit"></i></a>
+                        </a>
+
+                        <a href="#" class="btn btn-danger">
+                            <i class="fa fa-trash"></i></a>
+                        </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#" class="btn btn-primary">
+                            <i class="fa fa-edit"></i></a>
+                        </a>
+
+                        <a href="#" class="btn btn-danger">
+                            <i class="fa fa-trash"></i></a>
+                        </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>183</td>
+                    <td>John Doe</td>
+                    <td>11-7-2014</td>
+                    <td><span class="tag tag-success">Approved</span></td>
+                    <td>
+                        <a href="#" class="btn btn-primary">
+                            <i class="fa fa-edit"></i></a>
+                        </a>
+
+                        <a href="#" class="btn btn-danger">
+                            <i class="fa fa-trash"></i></a>
+                        </a>
+                    </td>
+                  </tr>
                  
                 </tbody></table>
               </div>
@@ -54,7 +99,37 @@
                 </button>
               </div>
               <div class="modal-body">
-                ...
+                <div class="form-group">
+                   <input v-model="form.name" type="text" name="name"
+                     class="form-control" :class="{ 'is-invalid': form.errors.has('name') }" placeholder="Enter Name">
+                   <has-error :form="form" field="name"></has-error>
+                </div> 
+                <div class="form-group">
+                   <input v-model="form.email" type="email" name="email"
+                     class="form-control" :class="{ 'is-invalid': form.errors.has('email') }" placeholder="Enter Email">
+                   <has-error :form="form" field="email"></has-error>
+                </div> 
+                <div class="form-group">
+                   <input v-model="form.password" type="password" name="password"
+                     class="form-control" :class="{ 'is-invalid': form.errors.has('password') }" placeholder="Enter Password">
+                   <has-error :form="form" field="password"></has-error>
+                </div>
+                <div class="form-group">
+                   <select v-model="form.type" name="type" id="type" 
+                     class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                     <option value="">Select User Role</option>
+                     <option value="admin">Admin</option>
+                     <option value="user">Standard User</option>
+                     <option value="author">Author</option>
+                   </select>
+                   <has-error :form="form" field="type"></has-error>
+                </div>
+                <div class="form-group">
+                   <textarea v-model="form.bio" name="bio" id="bio" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }" placeholder="Enter Bio (Optional)"></textarea>
+                   <has-error :form="form" field="bio"></has-error>
+                </div>
+                
+
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
@@ -68,6 +143,18 @@
 
 <script>
     export default {
+         data(){
+            return {
+              form: new Form({
+                name:'',
+                email:'',
+                password:'',
+                type:'',
+                bio:'',
+                photo:''
+              })
+            }
+         },
         mounted() {
             console.log('Component mounted.')
         }
